cf5fb09b59194f1faae7b189c9bfb832b066e3ff
Max <dannyjimenez12345@gmail.com>
2023-01-09 22:35:04 -0600

    Create function that changes the color of each div when the mouse passes over them

--- index.js
+++ index.js
@@ -4,7 +4,19 @@ function makeRows (rows, cols) {
     for (i=0; i < (rows*cols); i++) {
         const div = document.createElement("div");
         container.appendChild(div).className = "grid-items";
-    };
-};
+    }
+}
 
 makeRows(16, 16);
+
+
+function hoverColor() {
+    let items = document.querySelectorAll('.grid-items');
+    items.forEach(item => {
+        item.addEventListener('mouseover', () => {
+            item.style.backgroundColor = 'orange';
+        });
+    });
+}
+
+hoverColor();
