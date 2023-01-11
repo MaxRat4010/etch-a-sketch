918ebbdf26923d871d848fcbe1f7fec0624c838e
Max <dannyjimenez12345@gmail.com>
2023-01-11 00:19:46 -0600

    Rewrite makeRows function to create square divs inside the function instead of in CSS file

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const container = document.getElementById("container");
 
-function makeRows (rows, cols) {
-    for (i=0; i < (rows*cols); i++) {
-        const div = document.createElement("div");
-        container.appendChild(div).className = "grid-items";
+function makeRows (numberPerRow) {
+    const total = (numberPerRow * numberPerRow) + numberPerRow;
+    const box = numberPerRow + 1;
+
+    for (i=0; i < total; i++) {
+        const div = document.createElement('div');
+        container.appendChild(div).className = "gridItems";
+
+        if (i % box === 0) {
+        div.style.cssText = "border: 0; height: 0; width: 100%";
+        } else {
+        div.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
+        }
     }
 }
 
-makeRows(16, 16);
+makeRows(16);
 
 
 function hoverColor() {
-    let items = document.querySelectorAll('.grid-items');
+    let items = document.querySelectorAll('.gridItems');
     items.forEach(item => {
         item.addEventListener('mouseover', () => {
             item.style.backgroundColor = 'orange';
