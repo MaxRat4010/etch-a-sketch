7e669151fa484ed8cf9acf05374e42ec1c7a124c
Max <dannyjimenez12345@gmail.com>
2023-01-12 21:31:15 -0600

    Create clearGrid function and call it inside makeRow function

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 const container = document.getElementById("container");
+const button = document.querySelector("input");
+
+button.addEventListener('click', ()=> {
+    numOfSquares();
+})
+
+function numOfSquares() {
+    if (button.value === "number of squares") {
+        let newNumPerRow = prompt("how many squares per side would you like?", "");
+        let parse = parseInt(newNumPerRow);
+        makeRows(parse);
+    }
+}
+
+function hoverColor() {
+    let items = document.querySelectorAll('.gridItems');
+    items.forEach(item => {
+        item.addEventListener('mouseover', () => {
+            item.style.backgroundColor = 'orange';
+        });
+    });
+}
+
+function clearGrid() {
+    const gridArray = Array.from(container.childNodes);
+    gridArray.forEach(element => {
+      container.removeChild(element);
+    })
+  }
 
 function makeRows (numberPerRow) {
+    clearGrid();
     const total = (numberPerRow * numberPerRow) + numberPerRow;
     const box = numberPerRow + 1;
 
@@ -13,31 +43,9 @@ function makeRows (numberPerRow) {
         } else {
         div.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
         }
+
     }
+    hoverColor();
 }
 
 makeRows(16);
-
-
-function hoverColor() {
-    let items = document.querySelectorAll('.gridItems');
-    items.forEach(item => {
-        item.addEventListener('mouseover', () => {
-            item.style.backgroundColor = 'orange';
-        });
-    });
-}
-
-hoverColor();
-
-const button = document.querySelector("input");
-
-button.addEventListener('click', () => {
-    numOfSquares();
-});
-
-function numOfSquares() {
-    if (button.value === "number of squares") {
-        prompt("how many squares per side would you like?", "");
-    }
-}
